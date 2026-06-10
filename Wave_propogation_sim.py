174a857c93dc4e1e0273aa20593cc469e554a9d1
Ramsey Stiles <stilra7@furman.edu>
2026-06-10 10:35:13 -0400

    Refactor wave propagation simulation with optimizations

--- Wave_propogation_sim.py
+++ Wave_propogation_sim.py
@@ -18,6 +18,14 @@ dt = 20*10**-18
 # x array
 x = np.arange(-20e-6,20e-6,dx)
 
+v = x.copy()*0
+for i in range(len(v)):
+    if x[i] > -5e-6 and x[i] < 5e-6:
+        v[i]= 2.9979e8/1.5
+    else:
+        v[i]= 2.9979e8
+
+
 # T array
 t= np.arange(0, 1e-15, dt)
 
@@ -34,20 +42,19 @@ def Calc_E(x,t):
 
     return E
 
-def Ef(E0,E1):
+def Ef(E0,E1, optimizer = 1):
     '''Uses 4th order finite differencing to calculate future values of E'''
-    E2 = []
-    for i in range(len(E1[:-2])):
-        #loops through and determines where the dielectric is and changes the speed of light accordingly
-        if x[i] > -5e-6 and x[i] < 5e-6:
-            c = 2.9979e8/1.5
-        else:
-            c = 2.9979e8
-        E2.append((((c**2)*(-E1[i+2]+(16*E1[i+1])-30*E1[i]+16*E1[i-1]-E1[i-2])*(dt**2)/(12*(dx**2)))+2*E1[i])-E0[i])
-    #appends 2 dummy values to list to accound for losing 2 values with 4th order finite differencing
-    E2.append(0)
-    E2.append(0)
-    return E2
+    for opt in range(optimizer):
+        E2 = [0,0]
+        for i in range(2,len(E1[:-2])):
+            E2.append((((v[i]**2)*(-E1[i+2]+(16*E1[i+1])-30*E1[i]+16*E1[i-1]-E1[i-2])*(dt**2)/(12*(dx**2)))+2*E1[i])-E0[i])
+        #appends 2 dummy values to list to accound for losing 2 values with 4th order finite differencing
+        E2.extend([0,0])
+
+        E0 = E1.copy()
+        E1 = E2.copy()
+
+    return E0,E1
 
 #initializes first 2 values of E
 E0 = Calc_E(x,t[0])
@@ -70,7 +77,7 @@ def update(frame):
     ''' update function (Game Loop) that continuously gets called to produce a future value for E updating'''
     global E0, E1
 
-    E2 = Ef(E0, E1)
+    E1,E2 = Ef(E0, E1, 10)
 
     E0 = E1.copy()
     E1 = E2.copy()
@@ -81,7 +88,7 @@ def update(frame):
 ani = FuncAnimation(
     fig,
     update, 
-    frames=5000, 
+    frames=650, 
     interval=1, 
     blit=True
 )
